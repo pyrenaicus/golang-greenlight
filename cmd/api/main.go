06771fe22edb0e68aec0e2a8471b30e08a5afb4e
Pau Guerrero <pauguerrero@gmail.com>
2026-03-11 15:45:00 +0100

    configure server with endpoint /v1/healthcheck

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -14,15 +14,15 @@ const version = "1.0.0"
 // a struct holding all the configuration settings for our app.
 // network port & current operating environment.
 type config struct {
-	port	int
-	env		string
+	port int
+	env  string
 }
 
 // a struct holding the dependencies for our HTTP handelrs, helpers
 // and middleware.
 type application struct {
-	config	config
-	logger	*slog.Logger
+	config config
+	logger *slog.Logger
 }
 
 func main() {
@@ -35,10 +35,28 @@ func main() {
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 
 	app := &application{
-		config:	cfg,
+		config: cfg,
 		logger: logger,
 	}
 
+	// declare a servemux and add a route
 	mux := http.NewServeMux()
-	mux.
+	mux.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
+
+	// declare a HTTP server
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", cfg.port),
+		Handler:      mux,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		ErrorLog:     slog.NewLogLogger(logger.Handler(), slog.LevelError),
+	}
+
+	// start the HTTP server
+	logger.Info("starting server", "addr", srv.Addr, "env", cfg.env)
+
+	err := srv.ListenAndServe()
+	logger.Error(err.Error())
+	os.Exit(1)
 }
