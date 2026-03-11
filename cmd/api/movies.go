e5bee8d5aeb2955174886619d9e554d660cc0503
Pau Guerrero <pauguerrero@gmail.com>
2026-03-11 18:58:16 +0100

    feat: add two movie handlers

---
+++ cmd/api/movies.go
@@ -0,0 +1,23 @@
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(w, "create a new movie")
+}
+
+func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
+	params := httprouter.ParamsFromContext(r.Context())
+
+	id, err := strconv.Atoi(params.ByName("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "show the details of movie %d\n", id)
+}
