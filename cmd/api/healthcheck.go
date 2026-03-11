9a337a42121a80db6f05baf1b4534f2249451a6d
Pau Guerrero <pauguerrero@gmail.com>
2026-03-11 16:01:52 +0100

    fix: use fmt.Fprintf instead of fmt.Fprintln

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -7,6 +7,6 @@ import (
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "status: available")
-	fmt.Fprintln(w, "environment: %s\n", app.config.env)
-	fmt.Fprintln(w, "version: %s\n", version)
+	fmt.Fprintf(w, "environment: %s\n", app.config.env)
+	fmt.Fprintf(w, "version: %s\n", version)
 }
