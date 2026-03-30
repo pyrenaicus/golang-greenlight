5143b981d62554e28aed1edb5f777669e3ad35b4
Pau Guerrero <pauguerrero@gmail.com>
2026-03-30 16:56:30 +0200

    feat(data): update Movie struct to use custom Runtime struct

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -9,7 +9,7 @@ type Movie struct {
 	CreatedAt time.Time `json:"-"`
 	Title     string    `json:"title"`
 	Year      int       `json:"year,omitzero"`
-	Runtime   int       `json:"runtime,omitzero"` // movie runtime in minutes
+	Runtime   Runtime   `json:"runtime,omitzero"` // movie runtime in minutes
 	Genres    []string  `json:"genres,omitzero"`
 	Version   int       `json:"version"` // starts at 1 and increments each
 	// time the movie information is updated
