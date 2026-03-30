3501684f2140a2421d6214c844e4eb39b1eeb966
Pau Guerrero <pauguerrero@gmail.com>
2026-03-30 18:01:04 +0200

    feat (api): configure httprouter instance to use our custom error hanlders

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -9,6 +9,15 @@ import (
 func (app *application) routes() http.Handler {
 	router := httprouter.New()
 
+	// convert the notFoundResponse() helper to a http.Handler using the
+	// http.HandlerFunc() adapter, and then set it as the custom error
+	// handler for 404 Not Found responses.
+	router.NotFound = http.HandlerFunc(app.notFoundResponse)
+
+	// convert the methodNotAllowedResponse() helper to a http.Handler
+	// and set it as custom error handler for 405 Method Not Allowd responses.
+	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
+
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
