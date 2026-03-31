8d1dab7741ea5280a799826ef2270e9e7d00a84d
Pau Guerrero <pauguerrero@gmail.com>
2026-03-31 12:00:47 +0200

    fix (api): add correct comparison operator

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -12,16 +12,16 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 			// use the built-in recover() fn to check if a panic occurred. If so, it
 			// will return the panic value. If not, it will return nil.
 			pv := recover()
-			if pv := nil {
+			if pv != nil {
 				// if there was a panic, set a "Connection: close" header on the response.
 				// This acts as a trigger to make Go's HTTP server automatically close
 				// the current connection after the response has ben sent.
 				w.Header().Set("Connection", "close")
 				// the value returned by recover() has the type any, so we use fmt.Errorf()
 				// with the %v verb to coerce it into an error and call out
-				// serverErrorResponse() helper. This will log the error at the ERROR 
+				// serverErrorResponse() helper. This will log the error at the ERROR
 				// level and send the client a 500 Internal Server Error response.
-				app.serverErrorResponse(w, r, fmt.Errorf("%v", pv))	
+				app.serverErrorResponse(w, r, fmt.Errorf("%v", pv))
 			}
 		}()
 
