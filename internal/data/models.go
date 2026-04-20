5c71eaa2943ae3c2de5aa323f06a526e7834e6ab
Pau Guerrero <pauguerrero@gmail.com>
2026-04-20 16:32:11 +0200

    feat (data): wrap MovieModel in a parent Models struct

---
+++ internal/data/models.go
@@ -0,0 +1,25 @@
+package data
+
+import (
+	"database/sql"
+	"errors"
+)
+
+// ErrRecordNotFound, we'll return this from Get() when looking up a movie
+// that doesn't exist on our database.
+var (
+	ErrRecordNotFound = errors.New("record not found")
+)
+
+// create a Models struct which wraps all our models.
+type Models struct {
+	Movies MovieModel
+}
+
+// for ease of use, we add a New() method which returns a Models struct
+// containing the initialized MovieModel.
+func NewModels(db *sql.DB) Models {
+	return Models{
+		Movies: MovieModel{DB: db},
+	}
+}
