86a4768566173c794443b30e6ff39c18e2fc79ce
Pau Guerrero <pauguerrero@gmail.com>
2026-04-24 11:37:12 +0200

    feat (data): update GetAll() to interpolate the return values of sortColumn and sortDirection into SQL query

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"errors"
+	"fmt"
 	"time"
 
 	"greenlight.cnoua.org/internal/validator"
@@ -182,12 +183,15 @@ func (m MovieModel) Delete(id int) error {
 func (m MovieModel) GetAll(title string, genres []string, filters Filters) ([]*Movie, error) {
 	// Update the SQL query to include full-text search.
 	// @@ is the matches operator. @> is the contains operator for PostgreSQL arrays.
-	query := `
+	// Add an ORDER BY clause and interpolate the sort column and direction. Notice
+	// that we also include a secondary sort on the movie ID to ensure consistent
+	// ordering.
+	query := fmt.Sprintf(`
 		SELECT id, created_at, title, year, runtime, genres, version
 		FROM movies
 		WHERE (to_tsvector('simple', title) @@ plainto_tsquery('simple', $1) OR $1 = '')
 		AND (genres @> $2 OR $2 = '{}')
-		ORDER BY id`
+		ORDER BY %s %s, id ASC`, filters.sortColumn(), filters.sortDirection())
 
 	// Create a context with a 3-second timeout.
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
