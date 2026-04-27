7b8ed4f2401e8a4cae7c3e00e38f54e0d564747d
Pau Guerrero <pauguerrero@gmail.com>
2026-04-27 16:33:59 +0200

    feat (api): implement deletion of old limiters after 3 minutes

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"sync"
+	"time"
 
 	"github.com/tomasen/realip"
 	"golang.org/x/time/rate"
@@ -34,11 +35,39 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 }
 
 func (app *application) rateLimit(next http.Handler) http.Handler {
+	// Define a client struct to hold the rate limiter & last seen for each client.
+	type client struct {
+		limiter  *rate.Limiter
+		lastSeen time.Time
+	}
 	// Declare a mutex and a map to hold the clients IPs and rate limiters.
 	var (
-		mu      sync.Mutex
-		clients = make(map[string]*rate.Limiter)
+		mu sync.Mutex
+		// Update the map so the values are pointers to a client struct.
+		clients = make(map[string]*client)
 	)
+
+	// Launch a background goroutine which removes old entries from the clients
+	// map once every minute.
+	go func() {
+		for {
+			time.Sleep(time.Minute)
+
+			// Lock the mutex to prevent any rate limiter checks from happening while
+			// the cleanup is taking place.
+			mu.Lock()
+
+			// Loop through all clients. If they haven't been seen within the last 3 min,
+			// delete the corresponding entry from the map.
+			for ip, client := range clients {
+				if time.Since(client.lastSeen) > 3*time.Minute {
+					delete(clients, ip)
+				}
+			}
+			// Unlock the mutex when the cleanup is complete.
+			mu.Unlock()
+		}
+	}()
 
 	// The function we are returning is a closure, which 'closes over' the limiter
 	// variable.
@@ -52,11 +81,15 @@ func (app *application) rateLimit(next http.Handler) http.Handler {
 		// Check to see if the IP address already exists in the map. If it doesn't,
 		// initialize a new rate limiter and add the IP and limiter to the map.
 		if _, found := clients[ip]; !found {
-			clients[ip] = rate.NewLimiter(2, 4)
+			clients[ip] = &client{limiter: rate.NewLimiter(2, 4)}
 		}
+
+		// Update the last seen time for the client.
+		clients[ip].lastSeen = time.Now()
+
 		// Call the Allow() on the rate limiter for the current IP. If the Request
 		// isn't permitted, unlock the mutex and send a 429 Too Many Requests response.
-		if !clients[ip].Allow() {
+		if !clients[ip].limiter.Allow() {
 			mu.Unlock()
 			app.rateLimitExceededResponse(w, r)
 			return
