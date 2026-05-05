cbf07533df022d7837c5cc904a9a4814815d5f16
Pau Guerrero <pauguerrero@gmail.com>
2026-05-05 09:05:41 +0200

    feat (data): create Matches() method to check whether provided password matches stored one

--- internal/data/users.go
+++ internal/data/users.go
@@ -1,6 +1,7 @@
 package data
 
 import (
+	"errors"
 	"time"
 
 	"golang.org/x/crypto/bcrypt"
@@ -42,4 +43,20 @@ func (p *password) Set(plaintextPassword string) error {
 	p.hash = hash
 
 	return nil
+}
+
+// Matches() checks whether the provided plaintext password matches the hashed
+// password stored in the struct, returning true if it matches and false if not.
+func (p *password) Matches(plaintextPassword string) (bool, error) {
+	err := bcrypt.CompareHashAndPassword(p.hash, []byte(plaintextPassword))
+	if err != nil {
+		switch {
+		case errors.Is(err, bcrypt.ErrMismatchedHashAndPassword):
+			return false, nil
+		default:
+			return false, err
+		}
+	}
+
+	return true, nil
 }
