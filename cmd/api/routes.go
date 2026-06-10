b98505867ed9dbabd563da16fa345fbd64a6edd1
Pau Guerrero <pauguerrero@gmail.com>
2026-06-10 11:37:24 +0200

    feat (api): create route GET /debug/vars & mount expvar handler

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"expvar"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
@@ -31,6 +32,9 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodPut, "/v1/users/activated", app.activateUserHandler)
 
 	router.HandlerFunc(http.MethodPost, "/v1/tokens/authentication", app.createAuthenticationTokenHandler)
+
+	// Register a new GET /debug/vars endpoint pointing to expvar handler.
+	router.Handler(http.MethodGet, "/debug/vars", expvar.Handler())
 
 	// Wrap the router with the rateLimit() middleware.
 	return app.recoverPanic(app.enableCORS(app.rateLimit(app.authenticate(router))))
